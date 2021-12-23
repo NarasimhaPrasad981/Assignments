dca847cd766543d180c029fac6d02ff641ff37f7
NarasimhaPrasad981 <badiginchala.narasimhaprasad@thoughtfocus.com>
2021-12-23 09:25:12 +0530

    ExceptionHandling

---
+++ ExceptionHandling/src/com/ThoughtFocus/Exceptions/AirthemeticException.java
@@ -0,0 +1,12 @@
+package com.ThoughtFocus.Exceptions;
+
+public class AirthemeticException {
+
+	public static void main(String[] args) {
+		int a=10,b=0;
+		int div=a/b;
+		System.out.println(div);
+
+	}
+
+}
---
+++ ExceptionHandling/src/com/ThoughtFocus/Exceptions/ArrayIndexOutOfBounds.java
@@ -0,0 +1,11 @@
+package com.ThoughtFocus.Exceptions;
+
+public class ArrayIndexOutOfBounds {
+
+	public static void main(String[] args) {
+         int a[] = new int[5];
+         a[6]=10;
+         System.out.println("Exception occurs Array index out of bounds exception");
+	}     
+
+}
